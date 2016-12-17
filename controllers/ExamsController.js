757ed561df0d76c5ff8bbe53966e213fd008db6e
matir8 <martin.markov28@gmail.com>
2016-12-17 22:46:20 +0200

    teachers now can assign homeworks and exams

--- controllers/ExamsController.js
+++ controllers/ExamsController.js
@@ -1,25 +1,87 @@
 import { requester } from '../utils/requster.js';
 import { templates } from '../utils/templates.js';
+import { formHandler } from '../utils/formHandler.js';
+
+const examsUrl = 'https://elsyser.herokuapp.com/api/exams/';
 
 export function ExamsController() {
-    let examsUrl = 'https://elsyser.herokuapp.com/api/exams/',
-        getData = requester.getJSON(examsUrl),
+    let getData = requester.getJSON(examsUrl),
         getTemplate = templates.get('exams');
 
     Promise.all([getData, getTemplate])
         .then((result) => {
             let data = result[0],
-                hbTemplate = Handlebars.compile(result[1]);
-                
+                hbTemplate = Handlebars.compile(result[1]),
+                isTeacher = false,
+                token = window.localStorage.getItem('token');
+
             data.forEach((el) => {
                 if (el.details.length > 150) {
                     el.details = el.details.slice(0, 149) + '...';
                 }
             });
 
-            let template = hbTemplate(data);
+            if (token.length === 41 && token[40] === '1') {
+                isTeacher = true;
+            }
+
+            let template = hbTemplate({ exams: data, isTeacher: isTeacher });
             $('#content').html(template);
+
+            $('#add-exam').on('click', () => {
+                addExamController();
+            })
         }).catch((err) => {
             console.log(err);
         });
+}
+
+function addExamController() {
+    templates.get('add-exam')
+        .then((res) => {
+            let hbTemplate = Handlebars.compile(res),
+                template = hbTemplate();
+
+            $('#content').html(template);
+
+            $('#sandbox-container input').datepicker({
+                format: "yyyy-mm-dd",
+                todayBtn: "linked",
+                language: "bg",
+                orientation: "bottom auto",
+                calendarWeeks: true,
+                autoclose: true,
+                todayHighlight: true
+            });
+
+            formHandler();
+
+            $('#add-exam').on('click', () => {
+                postExam();
+            })
+        })
+}
+
+function postExam() {
+    let body = {
+        date: '',
+        subject: '',
+        topic: '',
+        clazz: ''
+    }
+
+    //TODO: Validate
+    body.date = $('#date').val();
+    body.subject = $('#subject').val();
+    body.topic = $('#topic').val();
+    body.clazz = $('#studentClassNumber').val() + $('#studentClassLetter').val();
+
+    requester.postJSON(examsUrl, body)
+        .then(() => {
+            toastr.success('Added exam successfully!');
+            ExamsController();
+        }).catch((err) => {
+            toastr.error('Couldn\'t add the exam');
+            console.log(err);
+        })
 }
--- controllers/HomeworksControllers/HomeworksController.js
+++ controllers/HomeworksControllers/HomeworksController.js
@@ -1,15 +1,19 @@
 import { requester } from '../../utils/requster.js';
 import { templates } from '../../utils/templates.js';
+import { formHandler } from '../../utils/formHandler.js';
+
+const homeworksUrl = 'https://elsyser.herokuapp.com/api/homeworks/';
 
 export function HomeworksController() {
-    let examsUrl = 'https://elsyser.herokuapp.com/api/homeworks/',
-        getData = requester.getJSON(examsUrl),
+    let getData = requester.getJSON(homeworksUrl),
         getTemplate = templates.get('homeworks');
 
     Promise.all([getData, getTemplate])
         .then((result) => {
             let data = result[0],
-                hbTemplate = Handlebars.compile(result[1]);
+                hbTemplate = Handlebars.compile(result[1]),
+                isTeacher = false,
+                token = window.localStorage.getItem('token');
 
             data.forEach((el) => {
                 if (el.details.length > 150) {
@@ -17,9 +21,65 @@ export function HomeworksController() {
                 }
             });
 
-            let template = hbTemplate(data);
+            if (token.length === 41 && token[40] === '1') {
+                isTeacher = true;
+            }
+
+            let template = hbTemplate({ homeworks: data, isTeacher: isTeacher });
             $('#content').html(template);
+
+            $('#add-homework').on('click', () => {
+                addHomeworkController();
+            })
         }).catch((err) => {
             console.log(err);
         });
+}
+
+function addHomeworkController() {
+    templates.get('add-homework')
+        .then((res) => {
+            let hbTemplate = Handlebars.compile(res),
+                template = hbTemplate();
+
+            $('#content').html(template);
+
+            $('#sandbox-container input').datepicker({
+                format: "yyyy-mm-dd",
+                todayBtn: "linked",
+                language: "bg",
+                orientation: "bottom auto",
+                calendarWeeks: true,
+                autoclose: true,
+                todayHighlight: true
+            });
+
+            formHandler();
+
+            $('#add-homework').on('click', () => {
+                postHomework();
+            })
+        })
+}
+
+function postHomework() {
+    let body = {
+        deadline: '',
+        subject: '',
+        clazz: ''
+    }
+
+    //TODO: Validate
+    body.deadline = $('#date').val();
+    body.subject = $('#subject').val();
+    body.clazz = $('#studentClassNumber').val() + $('#studentClassLetter').val();
+
+    requester.postJSON(homeworksUrl, body)
+        .then(() => {
+            toastr.success('Added homework successfully!');
+            ExamsController();
+        }).catch((err) => {
+            toastr.error('Couldn\'t add the homework');
+            console.log(err);
+        })
 }
