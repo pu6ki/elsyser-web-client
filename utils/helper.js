0a0bd19d309659bb7a4da87d4b1a1b67a0e34764
matir8 <martin.markov28@gmail.com>
2017-01-03 21:21:00 +0200

    clean-up

--- utils/helper.js
+++ utils/helper.js
@@ -1,13 +1,5 @@
 import { requester } from './requster.js';
 
-export function setUsernameToLocalSorage() {
-    let profileUrl = 'https://elsyser.herokuapp.com/api/profile/';
-
-    requester.getJSON(profileUrl)
-        .then((data) => {
-            localStorage.setItem('elsyser-username', data.username || data.user.username);
-        });
-}
 
 export function isTeacher(token) {
     if (token.length === 41 && token[40] === '1') {
