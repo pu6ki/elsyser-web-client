082d01d632aa9a719a635bd92adfdaff0bc9fb05
matir8 <martin.markov28@gmail.com>
2017-01-04 10:54:44 +0200

    fixed little bug

--- controllers/AuthControllers/LogoutController.js
+++ controllers/AuthControllers/LogoutController.js
@@ -4,6 +4,7 @@ export function LogoutController() {
     toastr.success('Logged-out successfully!');
     localStorage.removeItem('token');
     localStorage.removeItem('elsyser-username');
+    localStorage.removeItem('elsyser-id');
     HeaderController();
     window.location.href = '/#/home';
     window.location.reload(true);
