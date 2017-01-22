dfe890181bcb02c729104a8e739438ffd7b0b606
matir8 <martin.markov28@gmail.com>
2017-01-22 17:06:14 +0200

    configured jasmine and added a few tests

--- tests/spec/run.js
+++ tests/spec/run.js
@@ -1,4 +1,4 @@
-import Jasmine from 'jasmine'
+import Jasmine from '../../node_modules/jasmine/lib/jasmine.js'
 
 var jasmine = new Jasmine()
 jasmine.loadConfigFile('tests/spec/support/jasmine.json')
