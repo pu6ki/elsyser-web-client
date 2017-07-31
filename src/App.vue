1ab1a5e182ba57b9bd29af2f64212be5e4215559
matir8 <martin.markov28@gmail.com>
2017-07-31 14:47:04 +0300

    Added Bootstrap and some styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <router-view></router-view>
   </div>
 </template>
@@ -12,12 +12,169 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 5px;
+  padding: 0px;
+  padding-top: 70px;
+  background-color: ghostwhite;
 }
+
+h1,
+body,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-family: "Lato", "Helvetica Neue", Helvetica, Arial, sans-serif;
+}
+
+a {
+  color: black;
+  font-size: 16px;
+  text-decoration: none;
+}
+
+a:hover {
+  color: rebeccapurple;
+  text-decoration: none;
+}
+
+label {
+  font-size: 17px;
+}
+
+input {
+  color: #666666
+}
+
+thead {
+  color: white;
+  background-color: rebeccapurple;
+}
+
+hr {
+  max-width: 100%;
+}
+
+@media(max-width:767px) {
+  .intro-message {
+    padding-bottom: 15%;
+  }
+
+  .intro-message>h1 {
+    font-size: 3em;
+  }
+
+  ul.intro-social-buttons>li {
+    display: block;
+    margin-bottom: 20px;
+    padding: 0;
+  }
+
+  ul.intro-social-buttons>li:last-child {
+    margin-bottom: 0;
+  }
+
+  .intro-divider {
+    width: 100%;
+  }
+}
+
+@media(max-width:1199px) {
+  ul.banner-social-buttons {
+    float: left;
+    margin-top: 15px;
+  }
+}
+
+@media(max-width:767px) {
+  .banner h2 {
+    font-size: 3em;
+    margin: 0;
+    text-shadow: 2px 2px 3px rgba(0, 0, 0, 0.6);
+  }
+
+  ul.banner-social-buttons>li {
+    display: block;
+    margin-bottom: 20px;
+    padding: 0;
+  }
+
+  ul.banner-social-buttons>li:last-child {
+    margin-bottom: 0;
+  }
+
+  .nav-collapse.collapse {
+    height: auto !important;
+    overflow: visible !important;
+  }
+}
+
+.form-control {
+    border: 0.5px solid lightgray !important;
+    border-radius: 5px !important;
+    background-color: white;
+}
+
+.btn-primary a {
+    color: white;
+}
+
+.btn-primary:hover, .btn-primary a:hover {
+    color: gold;
+}
+
+.row {
+    margin: 0px;
+}
+
+.panel {
+	position: relative;
+    margin-left: 15px;
+    margin-right: 15px;
+}
+
+.content {
+    margin: 10%;
+}
+
+.section-heading {
+    color: rebeccapurple;
+}
+
+.btn-primary {
+    background-color: rebeccapurple;
+    color: white;
+}
+
+.btn-primary:hover .btn-primary:active {
+    background-color: white;
+    color: rebeccapurple;
+}
+
+.btn-primary:hover a {
+    color: white;
+}
+
+.btn-primary:active a {
+    color: white;
+}
+
+#wrapper {
+    max-width: 150vw;
+}
+
+#wrapper .form-wrapper {
+    text-align: center;
+    z-index: 0;
+    border-radius: 10px;
+    background-color: white;
+    padding: 2em;
+}
+
+#pwd-container .form-wrapper, #go-home-button a {
+    color: white;
+}
+
 </style>
