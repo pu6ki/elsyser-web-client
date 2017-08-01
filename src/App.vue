40e6affcabceac204c89b4b7c3b624a5b40124f0
matir8 <martin.markov28@gmail.com>
2017-08-01 12:43:10 +0300

    Moved the bootsrap link tag to index.html

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <router-view></router-view>
   </div>
 </template>
