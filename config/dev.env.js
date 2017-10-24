470da4f41a5ad109742c1ecc36bc2203b30c1276
matir8 <martin.markov28@gmail.com>
2017-10-24 18:13:09 +0300

    Config shanges

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,5 +2,7 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  SECRET: '"PuRpl3r@1N"',
+  API_URL: '"https://elsyser-dev.herokuapp.com/api"'
 })
